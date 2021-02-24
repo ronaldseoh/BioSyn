910953518e38048e6cd1b88465295dd1a0637a47
Ronald Seoh <ronaldseoh@icloud.com>
2021-02-24 13:39:39 -0500

    scripts filename changes

--- scripts/train-ncbi-disease-batch-16_embeds_viz.sh
+++ scripts/train-ncbi-disease-batch-16_embeds_viz.sh
@@ -5,7 +5,7 @@ conda activate biosyn
 
 module load cudnn/7.6-cuda_9.0 cuda90
 
-MODEL=biosyn-ncbi-disease-batch-8_debug
+MODEL=biosyn-ncbi-disease-batch-16_embeds_viz
 BIOBERT_DIR=./pretrained/pt_biobert1.1
 OUTPUT_DIR=./tmp/${MODEL}
 DATA_DIR=./datasets/ncbi-disease
@@ -17,8 +17,8 @@ python train.py \
     --output_dir ${OUTPUT_DIR} \
     --use_cuda \
     --topk 20 \
-    --epoch 2 \
-    --train_batch_size 8 \
+    --epoch 1 \
+    --train_batch_size 16 \
     --initial_sparse_weight 0 \
     --learning_rate 1e-5 \
     --max_length 25 \
