7370f6d13d8c1a94c244fad927e553324562e9a9
Ronald Seoh <ronaldseoh@icloud.com>
2021-02-25 02:02:30 -0500

    embeds_viz.py: min/max neighbor changes count; # of queries without any neighbor changes

--- embeds_viz.py
+++ embeds_viz.py
@@ -7,7 +7,11 @@ prev_topk_by_queries = np.load('initial_topk_by_queries.npy')
 
 member_embeds_avg_change_history = []
 nonmember_embeds_avg_change_history = []
-num_members_swapped_avg_history = []
+
+num_neighbors_swapped_avg_history = []
+num_neighbors_swapped_min_history = []
+num_neighbors_swapped_max_history = []
+no_neighbors_change_count_history = []
 
 for i in range(150):
     batch_members = set(np.load(str(i) + '_topk.npy').flatten())
@@ -31,20 +35,38 @@ for i in range(150):
     member_embeds_avg_change_history.append(member_embeds_changes_avg)
     nonmember_embeds_avg_change_history.append(nonmember_embeds_changes_avg)
     
-    num_members_swapped_by_queries = []
+    num_neighbors_swapped_by_queries = []
+    
+    no_neighbors_change_count = 0
     
     for j in range(len(batch_topk_by_queries)):
         nn_prev = set(prev_topk_by_queries[j])
         nn_cur = set(batch_topk_by_queries[j])
         
-        num_members_swapped_by_queries.append(len(nn_prev - nn_cur))
+        num_swapped = len(nn_prev - nn_cur)
         
-    num_members_swapped_avg = np.mean(num_members_swapped_by_queries)
-    num_members_swapped_avg_history.append(num_members_swapped_avg)
+        num_neighbors_swapped_by_queries.append(num_swapped)
+        
+        if num_swapped == 0:
+            no_neighbors_change_count = no_neighbors_change_count + 1
+        
+    num_neighbors_swapped_avg = np.mean(num_neighbors_swapped_by_queries)
+    num_neighbors_swapped_avg_history.append(num_neighbors_swapped_avg)
+    
+    num_neighbors_swapped_min = np.min(num_neighbors_swapped_by_queries)
+    num_neighbors_swapped_min_history.append(num_neighbors_swapped_min)
+    
+    num_neighbors_swapped_max = np.max(num_neighbors_swapped_by_queries)
+    num_neighbors_swapped_max_history.append(num_neighbors_swapped_max)
+
+    no_neighbors_change_count_history.append(no_neighbors_change_count)
     
     print("Member embedding avg change =", member_embeds_changes_avg)
     print("Non-member embedding avg change =", nonmember_embeds_changes_avg)
-    print("Avg. # of neighbors swapped per query =", num_members_swapped_avg)
+    print("Avg. # of neighbors swapped per query =", num_neighbors_swapped_avg)
+    print("Min # of neighbors swapped per query =", num_neighbors_swapped_min)
+    print("Max # of neighbors swapped per query =", num_neighbors_swapped_max)
+    print("# of no neighbor changes=", no_neighbors_change_count, "out of", len(batch_topk_by_queries))
     
     print()
     
@@ -53,7 +75,7 @@ for i in range(150):
 
 np.save('member_embeds_avg_change_history.npy', member_embeds_avg_change_history)
 np.save('nonmember_embeds_avg_change_history.npy', nonmember_embeds_avg_change_history)
-np.save('num_members_swapped_avg_history.npy', num_members_swapped_avg_history)
+np.save('num_neighbors_swapped_avg_history.npy', num_neighbors_swapped_avg_history)
 
 # Plot
 
@@ -77,9 +99,36 @@ plt.close()
 
 plt.figure()
 plt.xlabel('steps')
-plt.ylabel('# neighbors swapped (per query)')
+plt.ylabel('avg # neighbors swapped (per query)')
 
-plt.plot(list(range(len(num_members_swapped_avg_history))), num_members_swapped_avg_history)
+plt.plot(list(range(len(num_neighbors_swapped_avg_history))), num_neighbors_swapped_avg_history)
 
-plt.savefig('num_members_swapped_avg_history.png')
+plt.savefig('num_neighbors_swapped_avg_history.png')
+plt.close()
+
+plt.figure()
+plt.xlabel('steps')
+plt.ylabel('min # neighbors swapped (per query)')
+
+plt.plot(list(range(len(num_neighbors_swapped_min_history))), num_neighbors_swapped_min_history)
+
+plt.savefig('num_neighbors_swapped_min_history.png')
+plt.close()
+
+plt.figure()
+plt.xlabel('steps')
+plt.ylabel('max # neighbors swapped (per query)')
+
+plt.plot(list(range(len(num_neighbors_swapped_max_history))), num_neighbors_swapped_max_history)
+
+plt.savefig('num_neighbors_swapped_max_history.png')
+plt.close()
+
+plt.figure()
+plt.xlabel('steps')
+plt.ylabel('# of no neighbor changes')
+
+plt.plot(list(range(len(no_neighbors_change_count_history))), no_neighbors_change_count_history)
+
+plt.savefig('no_neighbors_change_count_history.png')
 plt.close()
